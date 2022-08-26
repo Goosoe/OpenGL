18d264c0582dfb04e7ff438bcf8f762b1d7ac81c
Goosoe <miguel.al.lourenco@gmail.com>
2022-08-26 22:53:01 +0100

    init commit

---
+++ premake5.lua
@@ -0,0 +1,46 @@
+-- premake5.lua
+workspace "opengl"
+    configurations { "Debug", "Release" }
+    platforms {"x32", "x64"}
+
+
+project "opengl"
+    kind "ConsoleApp"
+    location "build"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+    ignoredefaultlibraries { "MSVCRT" }
+
+    -- LINKING --
+    -- why libdirs is commented - https://premake.github.io/docs/libdirs
+--    libdirs {"$libs"}
+    links {"libs/glfw3.lib", "libs/glfw3_mt.lib", "libs/glfw3dll.lib"}
+
+--    This include dirs includes the .h without having the need to write directories 
+--    in your include in the code this breaks the other lib dependencies. Do not use
+--    includedirs{"include/**"}
+    includedirs{"include"}
+
+    files { "src/**.h", "src/**.hpp", "src/**.c", "src/**.cpp" }
+    files { "include/**.h", "include/**.hpp", "include/**.c", "include/**.cpp" }
+    removefiles "**/dummy.cpp"
+
+    -- FILTERS --
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
+    filter { "platforms:Win32" }
+        system "windows"
+        architecture "x86"
+
+    filter { "platforms:x64" }
+        system "windows"
+        architecture "x86_64"
+
+    -- deactivate the filters above so what we write below does not apply to the last filter only
+    filter {}
---
+++ run.bat
@@ -0,0 +1,3 @@
+premake vs2022
+msbuild ./build\opengl.vcxproj /p:Platform=x64
+.\bin\Debug\opengl.exe
